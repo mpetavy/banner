ce582b9b6bcb07567b5d4de231086e702e2e300f
mpetavy <mpetavy>
2023-12-22 14:32:44 +0100

    common@v1.6.36 update

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
-import(
-	"os"
+import (
 	"github.com/common-nighthawk/go-figure"
+	"os"
 )
 
 func main() {
 
-  myFigure := figure.NewFigure(os.Args[1],"", true)
-  myFigure.Print()
+	myFigure := figure.NewFigure(os.Args[1], "", true)
+	myFigure.Print()
 }
